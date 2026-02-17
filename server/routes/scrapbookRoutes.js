c4219ae6f620424552e861b5e0fdb0dd9c20abf0
bayenyen <bryneborinaga16@gmail.com>
2026-02-17 17:39:33 +0800

    greensss

--- server/routes/scrapbookRoutes.js
+++ server/routes/scrapbookRoutes.js
@@ -11,14 +11,14 @@ import {
 
 const router = express.Router();
 
+// Public routes (must be before /:id to avoid conflicts)
+router.post('/public/:shareToken', getPublicScrapbook);
+
 // Protected routes
 router.post('/', authMiddleware, createScrapbook);
 router.get('/', authMiddleware, getScrapbooks);
 router.get('/:id', authMiddleware, getScrapbookById);
 router.put('/:id', authMiddleware, updateScrapbook);
 router.delete('/:id', authMiddleware, deleteScrapbook);
-
-// Public routes
-router.post('/public/:shareToken', getPublicScrapbook);
 
 export default router;
